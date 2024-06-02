2ed8741be21d5478a3f2ff415a2b40d462f4356a
cohsh <82075327+cohsh@users.noreply.github.com>
2024-06-02 12:23:35 +0900

    :zap: removed QuadColorBox.css

--- components/QuadColorBox.tsx
+++ components/QuadColorBox.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import styles_colobox from '../app/styles/ColorBox.module.css';
-import styles_quadcolorbox from '../app/styles/QuadColorBox.module.css';
+import styles from '../app/styles/ColorBox.module.css';
 
 interface QuadColorBoxProps {
     colors: string[];
@@ -8,11 +7,11 @@ interface QuadColorBoxProps {
 
 const QuadColorBox: React.FC<QuadColorBoxProps> = ({ colors }) => {
     return (
-        <div className={styles_quadcolorbox.quadcolorbox}>
-            <div className={styles_colobox.colorbox} style={{ backgroundColor: colors[0] }}></div>
-            <div className={styles_colobox.colorbox} style={{ backgroundColor: colors[1] }}></div>
-            <div className={styles_colobox.colorbox} style={{ backgroundColor: colors[2] }}></div>
-            <div className={styles_colobox.colorbox} style={{ backgroundColor: colors[3] }}></div>
+        <div className={styles.quadcolorbox}>
+            <div className={styles.colorbox} style={{ backgroundColor: colors[0] }}></div>
+            <div className={styles.colorbox} style={{ backgroundColor: colors[1] }}></div>
+            <div className={styles.colorbox} style={{ backgroundColor: colors[2] }}></div>
+            <div className={styles.colorbox} style={{ backgroundColor: colors[3] }}></div>
         </div>
     );
 };
