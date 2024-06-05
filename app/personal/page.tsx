66e6c75e04e03f713a79a018b3cf0ece200f672c
cohsh <82075327+cohsh@users.noreply.github.com>
2024-06-06 08:53:10 +0900

    :zap: added personal palettes

--- app/personal/page.tsx
+++ app/personal/page.tsx
@@ -6,7 +6,11 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const palettes = [
-  { name: "Progress Report by Marp", colors: ['#008DBD', '#FDFCF5', '#6B818E', 'black'] },
+  { name: "Progress Report by Marp", colors: ['#008DBD', '#FDFCF5', '#6B818E', '#000000'] },
+  { name: "JPS 2021 Sep. (Poster)", colors: ['#004D80', '#006C65', '#B51700', '#5E5E5E', '#000000', '#FFFFFF'] },
+  { name: "JPS 2022 Mar. (Oral)", colors: ['#4082BE', '#EE220C']},
+  { name: "Master's Thesis Defense", colors: ['#328131', '#EE220C']},
+  { name: "Research Website", colors: ['#A4CA68', '#DAEAD0', '#43341B', '#005E15']}
 ];
 
 const Home: React.FC = () => {
