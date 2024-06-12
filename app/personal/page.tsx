e1fa8f3b35e5d88e9f86f1d399deba99d5dd6a6c
cohsh <82075327+cohsh@users.noreply.github.com>
2024-06-12 20:11:07 +0900

    :zap: added some personal palettes

--- app/personal/page.tsx
+++ app/personal/page.tsx
@@ -7,11 +7,14 @@ import "react-toastify/dist/ReactToastify.css";
 import Link from "next/link";
 
 const palettes = [
-  { name: "Progress Report by Marp", colors: ['#008DBD', '#FDFCF5', '#6B818E'] },
-  { name: "JPS 2021 Sep. (Poster)", colors: ['#004D80', '#006C65', '#B51700', '#5E5E5E'] },
+  { name: "Slide 2024 June", colors: ['#008DBD', '#FDFCF5', '#6B818E'] },
   { name: "JPS 2022 Mar. (Oral)", colors: ['#4082BE', '#EE220C']},
-  { name: "Master's Thesis Defense", colors: ['#328131', '#EE220C']},
-  { name: "Research Website", colors: ['#A4CA68', '#DAEAD0', '#43341B', '#005E15']}
+  { name: "Poster 2022 Feb.", colors: ['#004CC7', '#004D80']},
+  { name: "Research Website (2022-)", colors: ['#A4CA68', '#DAEAD0', '#43341B', '#005E15']},
+  { name: "JPS 2021 Sep. (Poster)", colors: ['#004D80', '#006C65', '#B51700', '#5E5E5E'] },
+  { name: "Poster 2020 Dec.", colors: ['#4B5D3F', '#D6D6D6']},
+  { name: "JPS 2020 Sep. (Oral)", colors: ['#005E15']},
+  { name: "Master's Thesis Defense (2020)", colors: ['#328131', '#EE220C', 'ED220D']}
 ];
 
 const Home: React.FC = () => {
